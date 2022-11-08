f31e400fd841f3f4a99ef0b88bf2441d32f70942
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-11-08 11:22:21 +0100

    update file

--- src/Components/UserList/UserList.js
+++ src/Components/UserList/UserList.js
@@ -2,19 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Hero from '../Hero';
-import img1 from '../../Assets/img1.jpg';
-import img2 from '../../Assets/img2.jpg';
-import img3 from '../../Assets/img3.jpg';
-import img4 from '../../Assets/img4.jpg';
-import img5 from '../../Assets/img5.jpg';
-import img6 from '../../Assets/img6.jpg';
-import img7 from '../../Assets/img7.jpg';
-import img8 from '../../Assets/img8.jpg';
-import img9 from '../../Assets/img9.jpg';
-import img10 from '../../Assets/img10.jpg';
-const UserList = () => {
-	const imageUrl = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
+import { imageUrl } from '../utils/images';
 
+const UserList = () => {
 	const [users, setUsers] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState(null);
@@ -43,13 +33,11 @@ const UserList = () => {
 			<Hero />
 			<div className='user--cards'>
 				{users &&
-					users.map((user, index) => {
+					users.map((user) => {
 						return (
-							<Link to={`/UserPage/${user.id}`} className='user--card'>
-								<div className='card-img'>
-									<img src={imageUrl[index]} alt='' />
-								</div>
-								<div key={user.id} user={user} className='userContent'>
+							<Link to={`/UserPage/${user.id}`} className='user--card' key={user.id}>
+								<img src={imageUrl[user.id - 1]} alt='' className='user--card__img' />
+								<div className='userContent'>
 									<h2>
 										<span>Name:</span> {user.name}
 									</h2>
--- src/Components/UserList/UserPage.js
+++ src/Components/UserList/UserPage.js
@@ -1,33 +1,22 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import img1 from '../../Assets/img1.jpg';
-import img2 from '../../Assets/img2.jpg';
-import img3 from '../../Assets/img3.jpg';
-import img4 from '../../Assets/img4.jpg';
-import img5 from '../../Assets/img5.jpg';
-import img6 from '../../Assets/img6.jpg';
-import img7 from '../../Assets/img7.jpg';
-import img8 from '../../Assets/img8.jpg';
-import img9 from '../../Assets/img9.jpg';
-import img10 from '../../Assets/img10.jpg';
+import { imageUrl } from '../utils/images';
 
 const UserPage = () => {
-	const imageUrl = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
-
 	const navigate = useNavigate();
-	const params = useParams();
+	const { userId } = useParams();
 
 	useEffect(() => {
 		fetchUsers();
-	}, [params]);
+	}, [userId]);
 
 	const [users, setUsers] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState(null);
 
 	const fetchUsers = async () => {
-		const singleUser = `https://jsonplaceholder.typicode.com/users/${params.userId}`;
+		const singleUser = `https://jsonplaceholder.typicode.com/users/${userId}`;
 		try {
 			setIsLoading(true);
 			const user = await axios.get(singleUser);
@@ -49,7 +38,7 @@ const UserPage = () => {
 				{users && (
 					<div className='user--card page'>
 						<div className='card-img'>
-							<img src='https://source.unsplash.com/200x100/?portrait' alt='' />
+							<img src={imageUrl[userId - 1]} alt='' />
 						</div>
 						<div key={users.id} className='userPageContent'>
 							<h2>
